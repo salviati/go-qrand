37afa9bb46070470a79c00b0affa8b018f8d9b00
Utkan Güngördü <utkan@freeconsole.org>
2011-08-14 13:28:13 +0900

    gofmt sources

--- example/example.go
+++ example/example.go
@@ -18,7 +18,6 @@
    51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-
 package main
 
 import (
@@ -30,43 +29,55 @@ import (
 )
 
 var (
-	user = flag.String("u", "", "Username for QRNG server")
-	pass = flag.String("p", "", "Password for QRNG server")
+	user      = flag.String("u", "", "Username for QRNG server")
+	pass      = flag.String("p", "", "Password for QRNG server")
 	cachesize = flag.Int("b", 16, "Buffer size")
-	host = flag.String("host", qrand.Host, "Host name")
-	port = flag.String("port", qrand.Port, "Port")
+	host      = flag.String("host", qrand.Host, "Host name")
+	port      = flag.String("port", qrand.Port, "Port")
 )
 
 func main() {
 	flag.Parse()
-	
+
 	if *user == "" || *pass == "" {
 		fmt.Println("Username and password are required parameters.\n\n Usage:")
 		flag.PrintDefaults()
 		os.Exit(0)
 	}
-	
- 	q, err := qrand.NewQRand(*user, *pass, *cachesize, *host, *port)
- 	if err != nil { log.Fatal(err) }
+
+	q, err := qrand.NewQRand(*user, *pass, *cachesize, *host, *port)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	i8, err := q.Int8()
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(i8)
-	
+
 	i16, err := q.Int16()
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(i16)
-	
+
 	i32, err := q.Int32()
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(i32)
-	
+
 	i64, err := q.Int64()
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(i64)
-	
+
 	buf := make([]byte, 16)
 	_, err = q.ReadBytes(buf)
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(buf)
 }
--- qrand/qrand.go
+++ qrand/qrand.go
@@ -26,7 +26,7 @@ import (
 	"net"
 	"fmt"
 	"bytes"
- 	"encoding/binary"
+	"encoding/binary"
 	"os"
 )
 
@@ -35,7 +35,7 @@ const (
 	Port = "1227"
 )
 
-var response = []string {
+var response = []string{
 	"OK",
 	"Service was shutting down",
 	"Server was/is experiencing internal errors",
@@ -45,8 +45,8 @@ var response = []string {
 	"Authentication failed",
 	"User quota exceeded",
 }
-    
-var remedy = []string {
+
+var remedy = []string{
 	"None",
 	"Try again later",
 	"Try again later",
@@ -57,13 +57,11 @@ var remedy = []string {
 	"Try again later, or contact service admin to increase your quota(s)",
 }
 
-
-
 type QRand struct {
-	buffer, b []byte
+	buffer, b  []byte
 	buffersize int
 	user, pass string
-	l sync.Mutex
+	l          sync.Mutex
 }
 
 // Read requests len(rand) bytes from QRNG server.
@@ -72,34 +70,41 @@ type QRand struct {
 // Try not to use this function —the function
 // you're looking for is probably ReadBytes().
 func (q *QRand) Read(rand []byte) (int, os.Error) {
-	if len(rand) == 0 { return 0, nil }
+	if len(rand) == 0 {
+		return 0, nil
+	}
 
-	c, err := net.Dial("tcp", net.JoinHostPort(Host,Port))
-	if err != nil { return 0, err }
+	c, err := net.Dial("tcp", net.JoinHostPort(Host, Port))
+	if err != nil {
+		return 0, err
+	}
 	defer c.Close()
 
-	
 	b := bytes.NewBuffer([]byte(""))
 	fmt.Fprintf(b, "%c", 0)
-	binary.Write(b, binary.BigEndian, uint16(len(q.user) + len(q.pass) + 6))
+	binary.Write(b, binary.BigEndian, uint16(len(q.user)+len(q.pass)+6))
 	fmt.Fprintf(b, "%c%s%c%s", len(q.user), q.user, len(q.pass), q.pass)
 	binary.Write(b, binary.BigEndian, uint32(len(rand)))
 	_, err = c.Write(b.Bytes())
-	if err != nil { return 0, err }
+	if err != nil {
+		return 0, err
+	}
 
 	msg := make([]byte, 6)
-	
+
 	_, err = c.Read(msg)
-	if err != nil { return 0, err }
+	if err != nil {
+		return 0, err
+	}
 	b = bytes.NewBuffer(msg)
 	var remedyCode, responseCode uint8
 	binary.Read(b, binary.BigEndian, &responseCode)
 	binary.Read(b, binary.BigEndian, &remedyCode)
-	
+
 	if responseCode != 0 || remedyCode != 0 {
 		return 0, os.NewError("qrand: " + response[responseCode] + ": " + remedy[remedyCode])
 	}
-	
+
 	var available uint32
 	binary.Read(b, binary.BigEndian, &available)
 	_, err = c.Read(rand[:available])
@@ -109,7 +114,9 @@ func (q *QRand) Read(rand []byte) (int, os.Error) {
 // ReadData tries to read len(b) bytes of data into b.
 // It returns the number of bytes actually read, which can be less than len(b).
 func (q *QRand) ReadBytes(p []byte) (int, os.Error) {
-	if len(p) == 0 { return 0, nil }
+	if len(p) == 0 {
+		return 0, nil
+	}
 
 	q.l.Lock()
 	defer q.l.Unlock()
@@ -120,7 +127,7 @@ func (q *QRand) ReadBytes(p []byte) (int, os.Error) {
 		q.b = q.b[len(p):]
 		return len(p), nil
 	}
-	
+
 	read := 0
 	// First empty the buffer
 	copy(p, q.b)
@@ -132,22 +139,26 @@ func (q *QRand) ReadBytes(p []byte) (int, os.Error) {
 	if len(p) > len(q.buffer) {
 		n, err := q.Read(p)
 		read += n
-		if err != nil { return read, err }
+		if err != nil {
+			return read, err
+		}
 		return read, err
 	}
-	
+
 	// Fill in the buffer, and read from it.
 	n, err := q.Read(q.buffer)
-	if err != nil { return n, err }
+	if err != nil {
+		return n, err
+	}
 	q.b = q.buffer[:n]
-	
+
 	if len(q.b) >= len(p) {
 		copy(p, q.b[:len(p)])
 		read += len(p)
 		q.b = q.b[len(p):]
 		return read, nil
 	}
-	
+
 	// Shouldn't happen normally
 	copy(p, q.b)
 	read += len(q.b)
@@ -158,8 +169,13 @@ func (q *QRand) ReadBytes(p []byte) (int, os.Error) {
 func (q *QRand) readBytes(n int) ([]byte, os.Error) {
 	rand := make([]byte, n)
 	read, err := q.ReadBytes(rand)
-	if err != nil {return rand, err}
-	if read != n { return rand, os.NewError(fmt.Sprintf("qrand: Receieved insufficient data; requested: %d, received: %d", n, read)) }
+	if err != nil {
+		return rand, err
+	}
+	if read != n {
+		emsg := fmt.Sprintf("qrand: Receieved insufficient data; requested: %d, received: %d", n, read)
+		return rand, os.NewError(emsg)
+	}
 	return rand, nil
 }
 
@@ -178,7 +194,9 @@ func (q *QRand) Int8() (int8, os.Error) {
 // Uint16 fetches 16-bit random data and returns it as uint16.
 func (q *QRand) Uint16() (r uint16, err os.Error) {
 	data, err := q.readBytes(2)
-	if err != nil { return 0, err }
+	if err != nil {
+		return 0, err
+	}
 	binary.Read(bytes.NewBuffer(data), binary.BigEndian, &r)
 	return r, err
 }
@@ -186,7 +204,9 @@ func (q *QRand) Uint16() (r uint16, err os.Error) {
 // Int16 fetches 16-bit random data and returns it as int16.
 func (q *QRand) Int16() (r int16, err os.Error) {
 	data, err := q.readBytes(2)
-	if err != nil { return 0, err }
+	if err != nil {
+		return 0, err
+	}
 	binary.Read(bytes.NewBuffer(data), binary.BigEndian, &r)
 	return r, err
 }
@@ -194,7 +214,9 @@ func (q *QRand) Int16() (r int16, err os.Error) {
 // Uint32 fetches 32-bit random data and returns it as uint32.
 func (q *QRand) Uint32() (r uint32, err os.Error) {
 	data, err := q.readBytes(4)
-	if err != nil { return 0, err }
+	if err != nil {
+		return 0, err
+	}
 	binary.Read(bytes.NewBuffer(data), binary.BigEndian, &r)
 	return r, err
 }
@@ -202,7 +224,9 @@ func (q *QRand) Uint32() (r uint32, err os.Error) {
 // Int32 fetches 32-bit random data and returns it as int32.
 func (q *QRand) Int32() (r int32, err os.Error) {
 	data, err := q.readBytes(4)
-	if err != nil { return 0, err }
+	if err != nil {
+		return 0, err
+	}
 	binary.Read(bytes.NewBuffer(data), binary.BigEndian, &r)
 	return r, err
 }
@@ -210,7 +234,9 @@ func (q *QRand) Int32() (r int32, err os.Error) {
 // Uint64 fetches 64-bit random data and returns it as uint64.
 func (q *QRand) Uint64() (r uint64, err os.Error) {
 	data, err := q.readBytes(8)
-	if err != nil { return 0, err }
+	if err != nil {
+		return 0, err
+	}
 	binary.Read(bytes.NewBuffer(data), binary.BigEndian, &r)
 	return r, err
 }
@@ -218,7 +244,9 @@ func (q *QRand) Uint64() (r uint64, err os.Error) {
 // Int64 fetches 64-bit random data and returns it as int64.
 func (q *QRand) Int64() (r int64, err os.Error) {
 	data, err := q.readBytes(8)
-	if err != nil { return 0, err }
+	if err != nil {
+		return 0, err
+	}
 	binary.Read(bytes.NewBuffer(data), binary.BigEndian, &r)
 	return r, err
 }
@@ -226,15 +254,19 @@ func (q *QRand) Int64() (r int64, err os.Error) {
 // Float32 fetches 32-bit random data and returns it as a float32 in [0.0,1.0)
 func (q *QRand) Float32() (r float32, err os.Error) {
 	n, err := q.Int32()
-	if err != nil { return 0, err }
-	return float32(n)/(1<<32-1-1), err
+	if err != nil {
+		return 0, err
+	}
+	return float32(n) / (1<<32 - 1 - 1), err
 }
 
 // Float64 fetches 64-bit random data and returns it as a float64 in [0.0,1.0)
 func (q *QRand) Float64() (r float64, err os.Error) {
 	n, err := q.Int64()
-	if err != nil { return 0, err }
-	return float64(n)/(1<<64-1-1), err
+	if err != nil {
+		return 0, err
+	}
+	return float64(n) / (1<<64 - 1 - 1), err
 }
 
 // NewQRand creates a new instances of Quantum Random Bit Generator client.
@@ -243,8 +275,14 @@ func (q *QRand) Float64() (r float64, err os.Error) {
 // When host and/or port are empty,
 // they are replaced by the default values, Host and Port.
 func NewQRand(user, pass string, buffersize int, host, port string) (*QRand, os.Error) {
-	if buffersize < 1 { return nil, os.NewError("qrand: buffersize is too small.") }
-	if host == "" { host = Host }
-	if port == "" { port = Port }
-	return &QRand{ user: user, pass: pass, buffer: make([]byte, buffersize) }, nil
+	if buffersize < 1 {
+		return nil, os.NewError("qrand: buffersize is too small.")
+	}
+	if host == "" {
+		host = Host
+	}
+	if port == "" {
+		port = Port
+	}
+	return &QRand{user: user, pass: pass, buffer: make([]byte, buffersize)}, nil
 }
